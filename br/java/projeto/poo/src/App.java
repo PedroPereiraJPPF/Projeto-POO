65b75220221252677b4645c688b9588373a41215
Pedro <joaopedropereira201@gmail.com>
2023-09-21 22:26:58 -0300

    Configuração inicial JavaFX

--- br/java/projeto/poo/src/App.java
+++ br/java/projeto/poo/src/App.java
@@ -1,42 +1,41 @@
 package br.java.projeto.poo.src;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
-import br.java.projeto.poo.models.DAO.ClienteDao;
-import br.java.projeto.poo.models.DAO.EnderecoDao;
-import br.java.projeto.poo.models.DAO.TelefoneDao;
-import br.java.projeto.poo.models.DAO.VeiculoDao;
-import br.java.projeto.poo.models.VO.ClienteVO;
-import br.java.projeto.poo.models.VO.EnderecoVO;
-import br.java.projeto.poo.models.VO.TelefoneVO;
-import br.java.projeto.poo.models.VO.VeiculoVO;
-
-class App {
-    public static void main(String[] args) {
-        try {
-            ClienteVO cv = new ClienteVO(6, "joao pedros", "123.456.789-53"); 
-            ClienteDao<ClienteVO> cd = new ClienteDao<ClienteVO>();
-
-            EnderecoVO ev = new EnderecoVO(1, "444555", null, "ruas", "B", "assu", "rn", "59650000");
-            EnderecoDao<EnderecoVO> Ed = new EnderecoDao<EnderecoVO>();
-
-            TelefoneVO tv = new TelefoneVO(5, "12345", null, "11111");
-            TelefoneDao<TelefoneVO> td = new TelefoneDao<TelefoneVO>();
-
-            VeiculoVO vv = new VeiculoVO(4, "ABC-4457", "azul", "modelo massa2", "123.456.789-53", "avião");
-            VeiculoDao<VeiculoVO> vd = new VeiculoDao<VeiculoVO>();
-            
-            cd.buscarPorCPF(cv);
-            cd.inserir(cv);
-            ResultSet telefones = td.listar();
-            while(telefones.next()) {
-                System.out.println(telefones.getString("numero"));
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+ 
+public class App extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
             }
-            vd.inserir(vv);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
+        });
+        
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+  
+  /*
+  Parent root = FXMLLoader.load(getClass().getResource("MainScene.fxml"));
+  Scene scene = new Scene(root);
+  */
+  
+  Scene scene = new Scene(root, 300, 250);
+  
+  primaryStage.setTitle("Hello World!");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+ 
+ public static void main(String[] args) {
+        launch(args);
     }
 }
