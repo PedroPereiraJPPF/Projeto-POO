15f999d88ca5fc1c936a0c90178e1fb13302f9ed
João Emanoel Soares Silva <jemanoel.je@gmail.com>
2023-09-29 18:10:31 -0300

    Update FuncionariosController.java

--- br/java/projeto/poo/controller/FuncionariosController.java
+++ br/java/projeto/poo/controller/FuncionariosController.java
@@ -2,41 +2,7 @@ package br.java.projeto.poo.controller;
 
 import javafx.scene.input.MouseEvent;
 
-public class FucnionariosController {
+public class FuncionariosController extends BaseController{
 
-    @FXML
-    void carregarAutomoveis(MouseEvent event) {
-
-    }
-
-    @FXML
-    void carregarClientes(MouseEvent event) {
-
-    }
-
-    @FXML
-    void carregarFuncionarios(MouseEvent event) {
-
-    }
-
-    @FXML
-    void carregarOrcamentos(MouseEvent event) {
-
-    }
-
-    @FXML
-    void carregarPecas(MouseEvent event) {
-
-    }
-
-    @FXML
-    void carregarServicos(MouseEvent event) {
-
-    }
-
-    @FXML
-    void logout(MouseEvent event) {
-
-    }
-
+    
 }
