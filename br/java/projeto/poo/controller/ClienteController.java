afb674b3d491151a382971b612064a1d89d16595
João Emanoel Soares Silva <jemanoel.je@gmail.com>
2023-10-01 19:40:13 -0300

    Edição no controller de clientes

--- br/java/projeto/poo/controller/ClienteController.java
+++ br/java/projeto/poo/controller/ClienteController.java
@@ -1,5 +1,18 @@
 package br.java.projeto.poo.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Popup;
+
 public class ClienteController extends BaseController {
 
+    @FXML
+
+    Popup CadastrarCliente = new Popup();
+    Button NovoCliente = new Button();
+
+    public void AbrirPopUp(ActionEvent e){
+        CadastrarCliente.show();
+    };
 }
